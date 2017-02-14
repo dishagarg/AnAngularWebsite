b9548d2f7ab7d0426a08128333e5591c87ae9191
Disha Garg <garg.disha8@gmail.com>
2017-02-14 03:52:36 -0800

    Angular support for forms and form validation (with JS)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -61,4 +61,28 @@ angular.module("confusionApp", []).controller("MenuController", ['$scope', funct
     $scope.toggleDetails = function () {
         $scope.showDetails = !$scope.showDetails;
     };
+}])
+
+.controller('ContactController', ['$scope', function($scope) {
+    $scope.feedback = {mychannel:"", firstname:"", lastname:"", agree:false, email:""};
+    $scope.channels = [{value:"tel", label:"Tel."},{value:"Email",label:"Email"}];
+    $scope.invalidChannelSelection = false;
+}])
+
+.controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function () {
+        console.log($scope.feedback);
+        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+            $scope.invalidChannelSelection = true;
+            console.log('incorrect');
+        }
+        
+        else {
+            $scope.invalidChannelSelection = false;
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            $scope.feedback.mychannel="";
+            $scope.feedbackForm.$setPristine();
+            console.log($scope.feedback);
+        }
+    };
 }]);
